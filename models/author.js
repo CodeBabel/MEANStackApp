0fb6858c51f9c841a52b3b0ef0a201d754cc45d2
kaviyakulothungan10 <kaviyakulothungan@gmail.com>
2019-01-08 16:26:30 -0600

    committing my changes

---
+++ models/author.js
@@ -0,0 +1,17 @@
+var mongoose = require("mongoose");
+
+var authorSchema = mongoose.Schema({
+name:{type:String},
+role:{type:String}
+});
+
+var Author = mongoose.model('Author', authorSchema);
+
+exports.seedAuthors = function() {
+Author.find({}).exec(function(error, collection){
+if(collection.length === 0) {
+Author.create({name:'Ravindranath Barathy',role:'admin1'});
+Author.create({name:'Kaviya Kulothungan',role:'admin2'});
+}
+})
+}
--- server.js
+++ server.js
@@ -1,4 +1,8 @@
-var express = require('express')
+var express = require('express');
+var mongoose = require('mongoose');
+var authormodel = require('./models/author.js');
+
+
 var app = express();
 
 app.set('views', __dirname);
@@ -7,9 +11,26 @@ app.set('view engine', 'jade');
 
 app.use(express.static(__dirname));
 
+app.get('/api/authors', function(req, res) {
+mongoose.model('Author').find({}).exec(function(error, collection) {
+res.send(collection);
+})
+})
+
 app.get('*', function(req,res){
   res.render('index');
 });
+
+mongoose.connect('mongodb://localhost/codebabeldashboard');
+
+var con = mongoose.connection;
+con.once('open', function() {
+console.log('connected to mongodb successfully!!!')
+authormodel.seedAuthors();
+console.log('updated the database')
+
+});
+
 
 
 app.listen(3000);
