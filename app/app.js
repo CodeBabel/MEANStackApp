b12cef9dc34f45497504e4b9a2ed5865aa76f84f
kaviyakulothungan10 <kaviyakulothungan@gmail.com>
2019-01-10 15:11:55 -0600

    updating app.js

--- app/app.js
+++ app/app.js
@@ -1,7 +1,12 @@
-angular.module('app',[]);
+//angular.module('app', []);
+angular.module('app', ['ngResource']);
 
-angular.module('app').controller('testCtrl', function($scope){
+
+// angular.module('app').controller('testCtrl', function($scope) {
+angular.module('app').controller('testCtrl', function($scope, $resource) {
 //$scope.test = "working!!!";
 
-$scope.authors = [{name: 'Ravindranath Barathy', role:'Admin1'},{name: 'Kaviya Kulothungan', role:'Admin2'},{name: 'Vivek Cherarajan', role:'Admin3'}];
+//$scope.authors = [{name: 'Ravindranath Barathy', role:'Admin1'},{name: 'Kaviya Kulothungan', role:'Admin2'},{name: 'Vivek Cherarajan', role:'Admin3'}];
+
+$scope.authors = $resource('/api/authors').query();
 });
