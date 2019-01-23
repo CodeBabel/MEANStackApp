7ffbb282221435d3d902cffa4d29f968751c7310
kaviyakulothungan10 <35080404+kaviyakulothungan10@users.noreply.github.com>
2019-01-22 20:04:13 -0600

    updating the code

--- test/authors-data-spec.js
+++ test/authors-data-spec.js
@@ -52,8 +52,8 @@ describe("get authors", function(){
     //mongoose.connect('mongodb://localhost/codebabeldashboard');
     describe("get authors", function(){
         it("should never be empty since authors are seeded", function(done){ 
-            //mongoose.connect('mongodb://localhost/codebabeldashboard', function(){
-            mongoose.connect('mongodb://userdev1:userdev1@ds153394.mlab.com:53394/codebabeldashboard', function(){
+            mongoose.connect('mongodb://localhost/codebabeldashboard', function(){
+            //mongoose.connect('mongodb://userdev1:userdev1@ds153394.mlab.com:53394/codebabeldashboard', function(){
                 //resetAuthors(function(){
                     //authorModel.seedAuthors(function() {
                         mongoose.model('Author').find({}).exec(function(error, authorsList) {
